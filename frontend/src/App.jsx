357018f0ed3f09d3fba078bf6ddfdaf39d3d3a6a
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-10-07 13:21:07 +0530

    feat: radix ui configure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import { Flex, Text, Button, Badge } from "@radix-ui/themes";
+
+export default function MyApp() {
+	return (
+		<Flex direction="column" gap="2">
+			<Text>Hello from Radix Themes :)</Text>
+			<Button>Let's go</Button>
+		</Flex>
+	);
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Theme>
     <App />
+    </Theme>
   </StrictMode>,
 )
